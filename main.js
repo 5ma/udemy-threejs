5363950fe8793316aaf1be9d399ff2de3a6ca8d5
5ma <bbxxuw@gmail.com>
2023-10-30 11:35:39 +0900

    シーン、カメラ、レンダラーを追加してブラウザに表示してみる

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-
 import * as THREE from 'three';
+
+let scene, camera, renderer;
+
+// シーンを追加する
+scene = new THREE.Scene();
+
+// カメラを追加
+camera = new THREE.PerspectiveCamera(
+  50, // 視野角
+  window.innerWidth / window.innerHeight, // アスペクト比
+  0.1, // 開始距離
+  1000 // 終了距離
+);
+
+// レンダラーを追加
+renderer = new THREE.WebGL1Renderer();
+// レンダラーのサイズを変更：画面サイズに合わせる
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+renderer.render(scene, camera);
