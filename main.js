9eef35b66ed64bf290d6d7bde11360942e9095bd
komakine-anna <anna.komakine@relic.co.jp>
2023-10-31 12:28:29 +0900

    3Dオブジェクトを作成してブラウザにレンダリングする

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ camera = new THREE.PerspectiveCamera(
   0.1, // 開始距離
   1000 // 終了距離
 );
+camera.position.set(0, 0, +500);
 
 // レンダラーを追加
 renderer = new THREE.WebGL1Renderer({
@@ -20,4 +21,20 @@ renderer = new THREE.WebGL1Renderer({
 // レンダラーのサイズを変更：画面サイズに合わせる
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// ジオメトリ(骨格のようなもの)を作成
+let ballGeometry = new THREE.SphereGeometry(
+  100, // 球体の半径
+  64,  // widthSegments（ポリゴンの数、増やせば増やすほど球体に近づく）
+  32   // heightSegment（ポリゴンの数、増やせば増やすほど球体に近づく）
+);
+// マテリアル(材質)を作成
+// MeshPhysicalMaterialは光源を必要とするmaterial
+let ballMaterial = new THREE.MeshPhysicalMaterial();
+// メッシュ化してみよう
+let ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
+// 作成したメッシュをシーン上に追加する
+scene.add(ballMesh);
+
+// レンダリングしてみよう
 renderer.render(scene, camera);
