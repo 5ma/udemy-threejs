dc9ee06e77fe907b3f7c64485ba81b9b17d1d359
komakine-anna <anna.komakine@relic.co.jp>
2023-11-02 00:16:25 +0900

    ページのロード完了後にThree.jsを実行する

--- main.js
+++ main.js
@@ -5,71 +5,78 @@ import earthTexture from './textures/earth.jpg';
 
 let scene, camera, renderer, pointLight, controls;
 
-// シーンを追加する
-scene = new THREE.Scene();
+window.addEventListener('load', init);
 
-// カメラを追加
-camera = new THREE.PerspectiveCamera(
-  50, // 視野角
-  window.innerWidth / window.innerHeight, // アスペクト比
-  0.1, // 開始距離
-  1000 // 終了距離
-);
-camera.position.set(0, 0, +500);
+function init() {
+  // シーンを追加する
+  scene = new THREE.Scene();
 
-// レンダラーを追加
-renderer = new THREE.WebGL1Renderer({
-  alpha: true, // 背景色を透過させる
-});
-// レンダラーのサイズを変更：画面サイズに合わせる
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio(window.devicePixelRatio);
-document.body.appendChild(renderer.domElement);
+  // カメラを追加
+  camera = new THREE.PerspectiveCamera(
+    50, // 視野角
+    window.innerWidth / window.innerHeight, // アスペクト比
+    0.1, // 開始距離
+    1000 // 終了距離
+  );
+  camera.position.set(0, 0, +500);
 
-// テクスチャを追加してみよう
-let texture = new THREE.TextureLoader().load(earthTexture);
+  // レンダラーを追加
+  renderer = new THREE.WebGL1Renderer({
+    alpha: true, // 背景色を透過させる
+  });
+  // レンダラーのサイズを変更：画面サイズに合わせる
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  document.body.appendChild(renderer.domElement);
 
-// ジオメトリ(骨格のようなもの)を作成
-let ballGeometry = new THREE.SphereGeometry(
-  100, // 球体の半径
-  64, // widthSegments（ポリゴンの数、増やせば増やすほど球体に近づく）
-  32 // heightSegment（ポリゴンの数、増やせば増やすほど球体に近づく）
-);
-// マテリアル(材質)を作成
-// MeshPhysicalMaterialは光源を必要とするmaterial
-let ballMaterial = new THREE.MeshPhysicalMaterial({
-  map: texture
-});
+  // テクスチャを追加してみよう
+  let texture = new THREE.TextureLoader().load(earthTexture);
 
-// メッシュ化してみよう
-let ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
-// 作成したメッシュをシーン上に追加する
-scene.add(ballMesh);
+  // ジオメトリ(骨格のようなもの)を作成
+  let ballGeometry = new THREE.SphereGeometry(
+    100, // 球体の半径
+    64, // widthSegments（ポリゴンの数、増やせば増やすほど球体に近づく）
+    32 // heightSegment（ポリゴンの数、増やせば増やすほど球体に近づく）
+  );
+  // マテリアル(材質)を作成
+  // MeshPhysicalMaterialは光源を必要とするmaterial
+  let ballMaterial = new THREE.MeshPhysicalMaterial({
+    map: texture
+  });
 
-// 平行光源を追加
-// 第一引数は光の色、第二引数は光の強さ
-let directionalLight = new THREE.DirectionalLight(0xffffff, 2); // 0xというのはこれから16新数を書きますよということを表す
-directionalLight.position.set(1, 1, 1);
-scene.add(directionalLight);
+  // メッシュ化してみよう
+  let ballMesh = new THREE.Mesh(ballGeometry, ballMaterial);
+  // 作成したメッシュをシーン上に追加する
+  scene.add(ballMesh);
 
-// ディレクショナルヘルパーを使って平行光源がどこにあるのかを特定する
-let directionalLightHelper = new THREE.DirectionalLightHelper(
-  directionalLight,
-  100
-);
-scene.add(directionalLightHelper);
+  // 平行光源を追加
+  // 第一引数は光の色、第二引数は光の強さ
+  let directionalLight = new THREE.DirectionalLight(0xffffff, 2); // 0xというのはこれから16新数を書きますよということを表す
+  directionalLight.position.set(1, 1, 1);
+  scene.add(directionalLight);
 
-// ポイント光源を追加してみよう
-pointLight = new THREE.PointLight(0x646cff, 10000);
-pointLight.position.set(-100, -100, 50);
-scene.add(pointLight);
+  // ディレクショナルヘルパーを使って平行光源がどこにあるのかを特定する
+  let directionalLightHelper = new THREE.DirectionalLightHelper(
+    directionalLight,
+    100
+  );
+  scene.add(directionalLightHelper);
 
-// ポイント光源がどこにあるのかを特定する
-let pointLightHelper = new THREE.PointLightHelper(pointLight, 10); // 第二引数はpointLightHelperの大きさを数値で指定
-scene.add(pointLightHelper);
+  // ポイント光源を追加してみよう
+  pointLight = new THREE.PointLight(0x646cff, 10000);
+  pointLight.position.set(-100, -100, 50);
+  scene.add(pointLight);
 
-// マウス操作ができるようにしよう
-controls = new OrbitControls(camera, renderer.domElement);
+  // ポイント光源がどこにあるのかを特定する
+  let pointLightHelper = new THREE.PointLightHelper(pointLight, 10); // 第二引数はpointLightHelperの大きさを数値で指定
+  scene.add(pointLightHelper);
+
+  // マウス操作ができるようにしよう
+  controls = new OrbitControls(camera, renderer.domElement);
+
+  animate();
+}
+
 
 function animate() {
   // ポイント光源を球の周りを巡回させる
@@ -83,5 +90,3 @@ function animate() {
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
 }
-
-animate();
